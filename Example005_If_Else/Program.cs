e6631982aa964236ac63376983efcce3d4b7f8d6
«annetta_tok» <annetta.gri@mail.ru>
2023-01-25 19:41:37 +0300

    Example 005 If Else

---
+++ Example005_If_Else/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "анечка")
+{
+    Console.WriteLine("Ура! Это Анечка");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+
+}
